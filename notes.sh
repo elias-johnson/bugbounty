3051e411c46b2243e66d065b00012884f040a5f2
elias-johnson <eliascarljohnson@gmail.com>
2025-07-13 14:02:47 -0700

    feat: improve message style

--- notes.sh
+++ notes.sh
@@ -9,7 +9,7 @@ function new_board() {
     ensure_board_does_not_exist "$1"
     echo -e "<board>$1\n</board>" >> ~/.notes
 
-    echo -e "board \033[1m$1\033[0m has been successfully made"
+    echo -e "board \033[38;5;153m\033[1m$1\033[0m has been successfully made"
 }
 
 function delete_board() {
@@ -20,7 +20,7 @@ function delete_board() {
     ensure_board_exists "$1"
     sed -i "/<board>$1$/,/<\/board>/d" ~/.notes
 
-    echo -e "board \033[1m$1\033[0m has been successfully deleted"
+    echo -e "board \033[38;5;153m\033[1m$1\033[0m has been successfully deleted"
 }
 
 function add() {
@@ -32,7 +32,7 @@ function add() {
 
     sed -i "/<board>$4$/,/<\/board>/s|</board>|<name>$1<\/name> <desc>$2<\/desc>\n</board>|" ~/.notes
 
-    echo -e "note \033[1m$1\033[0m has been successfully added to \033[1m$4\033[0m"
+    echo -e "note \033[38;5;223m\033[1m$1\033[0m has been successfully added to \033[38;5;153m\033[1m$4\033[0m"
 }
 
 function remove() {
@@ -44,29 +44,32 @@ function remove() {
 
     sed -i "/<board>$3$/,/<\/board>/ { /<name>$1<\/name>/d }" ~/.notes
 
-    echo -e "note \033[1m$1\033[0m has been successfully removed from \033[1m$3\033[0m"
+    echo -e "note \033[38;5;223m\033[1m$1\033[0m has been successfully removed from \033[38;5;153m\033[1m$3\033[0m"
 }
 
 function display_boards() {
-    echo "Current boards:"
     echo
-    sed -n "s|.*<board>\(.*\)|• \x1b[1m\1\x1b[0m|p" ~/.notes
+    echo -e "[ \033[1mall boards\033[0m ]"
+    echo
+    sed -n "s|.*<board>\(.*\)|• \x1b[38;5;153m\x1b[1m\1\x1b[0m|p" ~/.notes
     echo
 }
 
 function show() {
     if [[ -z $1 ]]; then
-        echo "All notes:"
         echo
-        sed -n "s|.*<name>\(.*\)</name>.*<desc>\(.*\)</desc>.*|• \x1b[1m\1\x1b[0m: \2|p" ~/.notes
+        echo -e "[ \033[1mall notes\033[0m ]"
+        echo
+        sed -n "s|.*<name>\(.*\)</name>.*<desc>\(.*\)</desc>.*|• \x1b[38;5;223m\x1b[1m\1\x1b[0m: \2|p" ~/.notes
+        echo
     else
         ensure_board_exists "$1"
-        echo -e "Notes on \033[1m$1\033[0m:"
         echo
-        # Clean this up
-        sed -n "/<board>$1$/,/<\/board>/p" ~/.notes | sed -n "s|.*<name>\(.*\)</name>.*<desc>\(.*\)</desc>.*|• \x1b[1m\1\x1b[0m: \2|p"
+        echo -e "[ \033[1m$1\033[0m ]"
+        echo
+        sed -n "/<board>$1$/,/<\/board>/p" ~/.notes | sed -n "s|.*<name>\(.*\)</name>.*<desc>\(.*\)</desc>.*|• \x1b[38;5;223m\x1b[1m\1\x1b[0m: \2|p" # Gross! Clean this up!
+        echo
     fi
-    echo
 }
 
 function help() {
